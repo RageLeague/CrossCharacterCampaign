10fae078320776b47506e233300a3ae9b04f6261
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-10-23 11:04:32 -0400

    Update add_coin_graft_to_reward.lua

--- add_coin_graft_to_reward.lua
+++ add_coin_graft_to_reward.lua
@@ -1,19 +1,65 @@
+-- This patch reworks the UI of Widget.PickGraftOption to allow more graft types
+-- to be displayed properly in the graft draft UI.
+-- For example, it can allow the UI for coin graft draft or social boon draft to
+-- be displayed properly.
+-- To use it, simply copy the content of this file to your own mod and load it.
+-- You may modify the content of the tables below to make it better suit your need.
+-- It is designed to be compatible with other mods that uses this patch.
+
+-- MIT License
+
+-- Copyright (c) 2021 RageLeague
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
 require "ui/widgets/optionpicker"
 require "ui/widgets/screen"
 require "ui/widgets/tooltip_graft"
 require "ui/screens/replacegraftscreen"
 
+-- Defines the colors for these graft types.
+-- Each entry can be an integer representing the color, or a function that takes
+-- a graft as a parameter and returns an integer representing the color.
+-- You may add additional entries here to expand to more graft types.
+-- The patch is loaded earlier takes precedence in terms of entries loaded.
 local GRAFT_COLORS = {
     [GRAFT_TYPE.NEGOTIATION] = UICOLOURS.NEGOTIATION,
     [GRAFT_TYPE.COMBAT] = UICOLOURS.FIGHT,
     [GRAFT_TYPE.COIN] = UICOLOURS.MONEY,
-    [GRAFT_TYPE.SOCIAL] = UICOLOURS.RELATIONSHIP_LOVED,
+    [GRAFT_TYPE.SOCIAL] = function(graft)
+        return graft:GetDef().is_good and UICOLOURS.RELATIONSHIP_LOVED or UICOLOURS.RELATIONSHIP_HATED
+    end,
 }
+
+-- Defines the name for these graft types.
+-- Each entry can be a string representing the localization ID of the text, or
+-- a function that takes a graft as a parameter and returns a string
+-- representing the localization ID of the text.
+-- You may add additional entries here to expand to more graft types.
+-- The patch is loaded earlier takes precedence in terms of entries loaded.
 local GRAFT_NAME = {
     [GRAFT_TYPE.NEGOTIATION] = "UI.GRAFT_COLLECTION.SLOT_TYPE_NEGOTIATION",
     [GRAFT_TYPE.COMBAT] = "UI.GRAFT_COLLECTION.SLOT_TYPE_COMBAT",
     [GRAFT_TYPE.COIN] = "UI.GRAFT_COLLECTION.SLOT_TYPE_COIN",
-    [GRAFT_TYPE.SOCIAL] = "UI.GRAFT_COLLECTION.SLOT_TYPE_SOCIAL_BOON",
+    [GRAFT_TYPE.SOCIAL] = function(graft)
+        return graft:GetDef().is_good and "UI.GRAFT_COLLECTION.SLOT_TYPE_SOCIAL_BOON" or "UI.GRAFT_COLLECTION.SLOT_TYPE_SOCIAL_BANE"
+    end,
 }
 
 local PickGraftOption = Widget.PickGraftOption
@@ -24,8 +70,15 @@ if not PickGraftOption.GRAFT_COLORS_MAP or not PickGraftOption.GRAFT_NAME_MAP th
         oldInitFunction(self, graft)
         local graftType = graft:GetType()
         local graftColor = graftType and self.GRAFT_COLORS_MAP[graftType] or UICOLOURS.FIGHT
+        if type(graftColor) == "function" then
+            graftColor = graftColor(graft)
+        end
         -- Not the name of the graft, the type name
-        local graftName = graftType and LOC(self.GRAFT_NAME_MAP[graftType]) or LOC"UI.GRAFT_COLLECTION.SLOT_TYPE_NONE"
+        local graftName = graftType and self.GRAFT_NAME_MAP[graftType]
+        if type(graftName) == "function" then
+            graftName = graftName(graft)
+        end
+        graftName = graftName and LOC(graftName) or LOC"UI.GRAFT_COLLECTION.SLOT_TYPE_NONE"
         self.bg:SetTintColour(graftColor)
         self.gradient:SetTintColour(graftColor)
         self.graft_frame:SetTintColour(graftColor)
